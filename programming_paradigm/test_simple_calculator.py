efa0668be6aa55e7c935a7ea9d3d5487dd508ec4
elzahawy <keyuuelzahawii@gmail.com>
2025-10-05 21:37:54 +0300

    Add test_multiplication and test_division methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,34 +3,27 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
-        """Set up calculator instance before each test"""
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        """Test addition"""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
-        """Test subtraction"""
         self.assertEqual(self.calc.subtract(5, 3), 2)
-        self.assertEqual(self.calc.subtract(3, 5), -2)
-        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(0, 4), -4)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
 
-    def test_multiply(self):
-        """Test multiplication"""
-        self.assertEqual(self.calc.multiply(4, 3), 12)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
-        self.assertEqual(self.calc.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    def test_divide(self):
-        """Test division"""
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-        self.assertEqual(self.calc.divide(9, 3), 3)
-
-        # division by zero must raise error
-        with self.assertRaises(ZeroDivisionError):
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        with self.assertRaises(ValueError):
             self.calc.divide(5, 0)
 
 if __name__ == "__main__":
