6fb4025917dbe3ba207084353659c0053cb5fb12
elzahawy <keyuuelzahawii@gmail.com>
2025-10-05 21:22:09 +0300

    Add SimpleCalculator class and unit tests

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,21 +3,33 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
+        """Set up calculator instance before each test"""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
+        """Test addition"""
         self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
+        """Test subtraction"""
         self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
+        """Test multiplication"""
         self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
+        """Test division"""
         self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(9, 3), 3)
 
-    def test_divide_by_zero(self):
+        # division by zero must raise error
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(5, 0)
 
